23e599e5dca8a4cb1fe2f017d02d66ac81225d0d
Semicolon <adedamola13@gmail.com>
2025-09-29 00:33:45 +0100

    editing to the subjects page

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,6 +1,6 @@
 import ContactInfo from "../components/Contacts/ContactInfo.jsx";
 import Header from "../components/Landingpage/Header.jsx";
-import DepartmentContacts from "../components/Contacts/DepartmentContacts.jsx";
+// import DepartmentContacts from "../components/Contacts/DepartmentContacts.jsx";
 import VisitInfo from "../components/Contacts/VisitInfo.jsx";
 import Footer from "../components/Footer.jsx";
 
@@ -9,7 +9,7 @@ const ContactUs = () => {
         <>
             <Header />
             <ContactInfo />
-            <DepartmentContacts />
+            {/* <DepartmentContacts /> */}
             <VisitInfo />
             <Footer />
         </>
